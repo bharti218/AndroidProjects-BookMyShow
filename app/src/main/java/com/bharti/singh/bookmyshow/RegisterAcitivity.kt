e3be469261ba99b1fd62f7575233554599353f2a
bhartiGetMega <bharti.singh@getmega.com>
2022-09-17 16:04:08 +0530

    [Android][BMS] added main activity

---
+++ app/src/main/java/com/bharti/singh/bookmyshow/RegisterAcitivity.kt
@@ -0,0 +1,11 @@
+package com.bharti.singh.bookmyshow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterAcitivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_acitivity)
+    }
+}
