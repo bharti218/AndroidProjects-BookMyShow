c689d2da0b0bc9226b40bd3fb945f1c0811e76bb
bhartiGetMega <bharti.singh@getmega.com>
2022-09-18 13:58:43 +0530

    [Android][BMS] login activity complete

--- app/src/main/java/com/bharti/singh/bookmyshow/LoginActivity.kt
+++ app/src/main/java/com/bharti/singh/bookmyshow/LoginActivity.kt
@@ -3,15 +3,19 @@ package com.bharti.singh.bookmyshow
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var loginBtn: Button
     private lateinit var email: EditText;
     private lateinit var password: EditText;
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +24,7 @@ class LoginActivity : AppCompatActivity() {
         email = findViewById(R.id.login_email_input)
         password = findViewById(R.id.login_password)
         loginBtn = findViewById(R.id.activity_login_btn)
+        firebaseAuth = FirebaseAuth.getInstance()
 
         loginBtn.setOnClickListener {
             login()
@@ -27,11 +32,28 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun login() {
-        startActivity(
-            Intent(
-                this,
-                MainActivity::class.java
-            ).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        )
+
+        val emailTxt = email.text.toString()
+        val passwordTxt = password.text.toString()
+
+        if (emailTxt.isEmpty() || passwordTxt.isEmpty())
+            Toast.makeText(this, "Invalid email or password", Toast.LENGTH_LONG).show()
+        else
+            loginUser(emailTxt, passwordTxt)
+    }
+
+    private fun loginUser(email: String, password: String) {
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+            if (it.isSuccessful) {
+                startActivity(
+                    Intent(
+                        this,
+                        MainActivity::class.java
+                    ).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                )
+            }
+        }.addOnFailureListener {
+            Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
+        }
     }
 }
